06ca1ddccf43287762033f699157bdfb5d4ed9e3
pelumi omotesho <pelumi066@gmail.com>
2018-07-11 21:58:54 +0100

    added join page

---
+++ playgrounds/time.js
@@ -0,0 +1,10 @@
+let moment = require('moment');
+
+// let date = moment();
+// date.add(7,'years')
+// console.log(date.format('MMM Do, YYYY'));
+
+let date = moment();
+console.log(date.format('hh:mm a'));
+
+
